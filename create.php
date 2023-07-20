543a9fe6f2e0eead0e596d9b49657c55368dba19
rovedabr <rovedabr@gmail.com>
2023-07-20 14:27:14 -0300

    started project

--- create.php
+++ create.php
@@ -5,6 +5,7 @@
   <h1>Teste de agenda</h1>
   <i class="fas fa-eye" id="eye"></i>
   <i class="fa-solid fa-house" id="house"></i>
+  <i class="fa-solid fa-house" id="house"></i>
 
   <?php
     include_once("templates/footer.php");
